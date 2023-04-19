2e9977fb4b15eef154c778a24b0579a5f6fd0e48
Andrew Korchevskiy <37622720+Salamandero@users.noreply.github.com>
2023-04-19 21:28:50 +0300

    Update RestricteRoute.js

--- src/components/RestricteRoute.js
+++ src/components/RestricteRoute.js
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom';
 
 const RestricteRoute = ({ component: Component, redirectTo = '/' }) => {
   // const { isLoggedIn } = useAuth();
-  const isLoggedIn = false;
+  const isLoggedIn = true;
   return isLoggedIn ? <Navigate to={redirectTo} /> : <Component />;
 };
 
