6b4487a4b4eee8f22e2fbd941ecc3fbe5cd47c37
AnastasiyaLytvyn <107053207+AnastasiyaLytvyn@users.noreply.github.com>
2023-04-22 11:42:25 +0300

    added redux for recipe page

--- src/pages/RecipePage/RecipePage.js
+++ src/pages/RecipePage/RecipePage.js
@@ -1,16 +1,30 @@
-// import { useEffect } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { getRecipeById } from 'redux/recipe/operations';
-
 import { RecipePageHero } from 'components/RecipePage/RecipePageHero/RecipePageHero';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getRecipeById } from 'redux/recipe/operations';
+import { selectRecipeById } from 'redux/recipe/selectors';
 
 const RecipePage = () => {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   dispatch(getRecipeById());
-  // }, [dispatch]);
+  const id = '640cd5ac2d9fecf12e8897fc';
+
+  useEffect(() => {
+    dispatch(getRecipeById(id));
+  }, [dispatch]);
+
+  const recipe = useSelector(selectRecipeById);
+  console.log(recipe);
   
-  return <RecipePageHero></RecipePageHero>;
+  const { title, description, time } = recipe.recipe;
+
+  return (
+    <RecipePageHero
+      title={title}
+      description={description}
+      time={time}
+    ></RecipePageHero>
+  );
 };
+
 export default RecipePage;
--- src/redux/recipe/recipeSlice.js
+++ src/redux/recipe/recipeSlice.js
@@ -1,32 +1,79 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { getRecipeById } from './operations';
-
-const handlePending = state => {
-  state.isLoading = true;
-};
-
-const handleRejected = (state, action) => {
-  state.isLoading = false;
-  state.error = action.payload;
-};
+import {
+  addIngredientToShoppingList,
+  addRecipeToFavorite,
+  getRecipeById,
+  removeIngredientFromShoppingList,
+  removeRecipeFromFavorite,
+} from './operations';
 
 const initialState = {
-  items: [],
-  isLoading: false,
-  error: null,
+  recipe: {
+    item: [],
+    isLoading: false,
+    error: null,
+  },
+  favorite: {
+    isFavorite: false,
+    isLoading: false,
+    error: null,
+  },
+  shoppingList: {
+    error: null,
+  },
 };
 
 const recipeSlice = createSlice({
   name: 'recipe',
   initialState: initialState,
-  extraReducers: {
-    [getRecipeById.pending]: handlePending,
-    [getRecipeById.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.items = action.payload;
-    },
-    [getRecipeById.rejected]: handleRejected,
+  extraReducers: builder => {
+    builder
+      .addCase(getRecipeById.pending, state => {
+        state.recipe.isLoading = true;
+      })
+      .addCase(getRecipeById.fulfilled, (state, action) => {
+        state.recipe.isLoading = false;
+        state.recipe.error = null;
+        state.recipe.item = action.payload;
+        state.favorite.isFavorite = action.payload.favorite;
+      })
+      .addCase(getRecipeById.rejected, (state, action) => {
+        state.recipe.isLoading = false;
+        state.recipe.error = action.payload;
+      })
+
+      .addCase(addRecipeToFavorite.pending, state => {
+        state.favorite.isLoading = true;
+      })
+      .addCase(addRecipeToFavorite.fulfilled, state => {
+        state.favorite.isFavorite = true;
+        state.favorite.isLoading = false;
+        state.favorite.error = false;
+      })
+      .addCase(addRecipeToFavorite.rejected, (state, action) => {
+        state.favorite.isLoading = false;
+        state.favorite.error = action.payload;
+      })
+
+      .addCase(removeRecipeFromFavorite.pending)
+      .addCase(removeRecipeFromFavorite.fulfilled, state => {
+        state.favorite.isFavorite = false;
+        state.favorite.isLoading = false;
+        state.favorite.error = false;
+      })
+      .addCase(removeRecipeFromFavorite.rejected)
+
+      .addCase(addIngredientToShoppingList.pending)
+      .addCase(addIngredientToShoppingList.fulfilled)
+      .addCase(addIngredientToShoppingList.rejected, (state, action) => {
+        state.shoppingList.error = action.payload;
+      })
+
+      .addCase(removeIngredientFromShoppingList.pending)
+      .addCase(removeIngredientFromShoppingList.fulfilled)
+      .addCase(removeIngredientFromShoppingList.rejected, (state, action) => {
+        state.shoppingList.error = action.payload;
+      });
   },
 });
 
