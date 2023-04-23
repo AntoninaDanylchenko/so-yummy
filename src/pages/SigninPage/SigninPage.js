335aec274cb13f23ed898c65a81d3ce6de082ed3
Andrew Korchevskiy <37622720+Salamandero@users.noreply.github.com>
2023-04-23 16:57:55 +0300

    a little fix header\register

--- src/pages/SigninPage/SigninPage.js
+++ src/pages/SigninPage/SigninPage.js
@@ -1,4 +1,5 @@
 // import { Helmet } from 'react-helmet-async';
+import { Link } from 'react-router-dom';
 
 import { Title, WrapperForm } from './SigninPage.styled';
 import { LoginForm } from '../../components/LoginForm/LoginForm';
@@ -12,6 +13,7 @@ const SigninPage = () => {
         </Helmet> */}
         <LoginForm />
       </WrapperForm>
+      <Link to="/register">Sign In</Link>
     </>
   );
 };
