a236b261c5ae3c2198c8af32452f7c574c68b1fb
Andrew Korchevskiy <37622720+Salamandero@users.noreply.github.com>
2023-04-24 17:13:34 +0300

    add style Register

--- src/redux/favorite/operation.js
+++ src/redux/favorite/operation.js
@@ -1,61 +1,59 @@
-import axios from 'axios';
-import { createAsyncThunk } from '@reduxjs/toolkit';
+// import axios from 'axios';
+// import { createAsyncThunk } from '@reduxjs/toolkit';
 
+// export const addFavoriteOp = createAsyncThunk(
+//     'fav/add',
+//     async (recipeId, thunkAPI) => {
+//         const state = thunkAPI.getState();
+//         // const persistedToken = state.auth.token;
+//         if (!persistedToken) {
+//             return thunkAPI.rejectWithValue('Unable to add favorite');
+//         }
+//         token.set(persistedToken);
+//         try {
+//             const r = await axios.post('/favorite', { recipeId });
 
-export const addFavoriteOp = createAsyncThunk(
-    'fav/add',
-    async (recipeId, thunkAPI) => {
-        const state = thunkAPI.getState();
-        // const persistedToken = state.auth.token;
-        if (!persistedToken) {
-            return thunkAPI.rejectWithValue('Unable to add favorite');
-        }
-        token.set(persistedToken);
-        try {
-            const r = await axios.post('/favorite', { recipeId });
+//             return r.data;
+//         } catch (e) {
+//             return thunkAPI.rejectWithValue(e.message);
+//         }
+//     }
+// );
 
-            return r.data;
-        } catch (e) {
-            return thunkAPI.rejectWithValue(e.message);
-        }
-    }
-);
+// export const getFavoriteOp = createAsyncThunk(
+//     'fav/get',
+//     async (_, thunkAPI) => {
+//         const state = thunkAPI.getState();
+//         // const persistedToken = state.auth.token;
+//         if (!persistedToken) {
+//             return thunkAPI.rejectWithValue('Unable to fetch favorite');
+//         }
+//         token.set(persistedToken);
+//         try {
+//             const r = await axios.get('/favorite');
 
-export const getFavoriteOp = createAsyncThunk(
-    'fav/get',
-    async (_, thunkAPI) => {
-        const state = thunkAPI.getState();
-        // const persistedToken = state.auth.token;
-        if (!persistedToken) {
-            return thunkAPI.rejectWithValue('Unable to fetch favorite');
-        }
-        token.set(persistedToken);
-        try {
-            const r = await axios.get('/favorite');
+//             return r.data.data;
+//         } catch (e) {
+//             return thunkAPI.rejectWithValue(e.message);
+//         }
+//     }
+// );
 
-            return r.data.data;
-        } catch (e) {
-            return thunkAPI.rejectWithValue(e.message);
-        }
-    }
-);
+// export const deleteFavoriteOp = createAsyncThunk(
+//     'fav/delete',
+//     async (recipeId, thunkAPI) => {
+//         const state = thunkAPI.getState();
+//         // const persistedToken = state.auth.token;
+//         if (!persistedToken) {
+//             return thunkAPI.rejectWithValue('Unable to delete recipe');
+//         }
+//         token.set(persistedToken);
+//         try {
+//             const r = await axios.delete('/favorite', { recipeId });
 
-export const deleteFavoriteOp = createAsyncThunk(
-    'fav/delete',
-    async (recipeId, thunkAPI) => {
-        const state = thunkAPI.getState();
-        // const persistedToken = state.auth.token;
-        if (!persistedToken) {
-            return thunkAPI.rejectWithValue('Unable to delete recipe');
-        }
-        token.set(persistedToken);
-        try {
-            const r = await axios.delete('/favorite', { recipeId });
-
-            return r.data;
-        } catch (e) {
-            return thunkAPI.rejectWithValue(e.message);
-        }
-    }
-);
-
+//             return r.data;
+//         } catch (e) {
+//             return thunkAPI.rejectWithValue(e.message);
+//         }
+//     }
+// );
