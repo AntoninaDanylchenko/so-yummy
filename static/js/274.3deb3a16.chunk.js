c4cd443e23c168000b1a8ebb0eb62c885e2ced07
Salamandero <37622720+Salamandero@users.noreply.github.com>
2023-04-25 11:10:52 +0000

    Deploying to gh-pages from @ AntoninaDanylchenko/so-yummy@90cff3c6c2646163141bcefe520e2709699d0166 🚀

--- static/js/274.3deb3a16.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkso_yummy=self.webpackChunkso_yummy||[]).push([[274],{4274:function(u,e,n){n.r(e);var s=n(184);e.default=function(){return(0,s.jsx)("div",{children:"MainPage"})}}}]);
-//# sourceMappingURL=274.3deb3a16.chunk.js.map
