fbecfd8b22a874bf4ef383d37e7994795bc9a686
Dmytro Melnyk <dmitromelnik96@gmail.com>
2023-04-25 22:33:14 +0300

    add modaluser

---
+++ src/components/UserModal/LogoutBtn/LogoutBtn.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import LogoutButton from './LogoutBtn.styled';
+
+
+const LogoutBtn = ({ onLogout }) => {
+  const handleLogout = async () => {
+    try {
+      await fetch('/logout', {
+        method: 'POST',
+        credentials: 'include',
+      });
+      onLogout();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <LogoutButton onClick={handleLogout}>
+      Logout
+    </LogoutButton>
+  );
+};
+
+export default LogoutBtn;
