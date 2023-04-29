bf73ba9bb51471edc39099d014ebdab3e5a39de9
SashaKrynytska <krynytskaya@gmail.com>
2023-04-29 14:36:55 +0300

    add logic to search

---
+++ src/components/SearchPage/SearchedRecipesList.js
@@ -0,0 +1,26 @@
+// import { RecipeItem } from 'components/RecipeItem/RecipeItem';
+// import PropTypes from 'prop-types';
+
+// export const SearchedRecipesList = ({ recipes }) => {
+//   console.log('recipes', recipes);
+//   return (
+//     <ul>
+//       {recipes?.map(recipe => (
+//         //    {recipes?.map(({ recipe: { _id, title, preview, thumb } }) => (
+//         <RecipeItem key={recipe._id} recipe={recipe} />
+//       ))}
+//     </ul>
+//   );
+// };
+
+// const recipesPropTypes = PropTypes.shape({
+//   _id: PropTypes.string.isRequired,
+//   title: PropTypes.string.isRequired,
+//   thumb: PropTypes.string.isRequired,
+// }).isRequired;
+
+// SearchedRecipesList.propTypes = {
+//   images: PropTypes.arrayOf(recipesPropTypes),
+// };
+
+// export default SearchedRecipesList;
--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -1,4 +1,99 @@
+import { Container } from '@mui/material';
+import { useState, useEffect } from 'react';
+// import { toast } from 'react-hot-toast';
+
+import PageTitle from 'components/ShoppingListPage/Title/PageTitle';
+import SearchBar from 'components/SearchPage/SearchBar';
+import fetchRecipesByIngredients, {
+  fetchRecipesByTitle,
+} from 'components/SearchPage/fetchAPI';
+import Button from 'components/SearchPage/ButtonLoad';
+import { SearchTypeSelector } from 'components/SearchPage/SearchTypeSelector';
+import { RecipeItem } from 'components/RecipeItem/RecipeItem';
+// import { RecipeItem } from 'components/RecipeItem/RecipeItem';
+
 const SearchPage = () => {
-  return <div>SearchPage</div>;
+  const [recipes, setRecipes] = useState([]);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [totalRecipes, setTotalRecipes] = useState(0);
+  const [isTitle, setIsTitle] = useState(true);
+
+  const onSelectChange = value => {
+    value === 'title' ? setIsTitle(true) : setIsTitle(false);
+  };
+
+  useEffect(() => {
+    if (!query) return;
+    setIsLoading(true);
+
+    (async () => {
+      try {
+        const response = isTitle
+          ? await fetchRecipesByTitle(query, page)
+          : await fetchRecipesByIngredients(query, page);
+        // console.log('response', response);
+        const dataRecipes = response.data.map(
+          ({ _id, title, preview, thumb }) => ({
+            id: _id,
+            title,
+            preview,
+            thumb,
+          })
+        );
+        console.log('dataRecipes', dataRecipes);
+        setRecipes(prevRecipes => [...prevRecipes, ...dataRecipes]);
+        setTotalRecipes(prevTotalRecipes => response.data.totalRecipes);
+
+        if (page !== 1) {
+          scrollOnLoadButton();
+        }
+      } catch (error) {
+        console.log('Something wrong... Try again!');
+        // setError({ error });
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, [isTitle, query, page]);
+
+  console.log('recipes', recipes);
+  console.log('isTitle', isTitle);
+
+  const onChangeQuery = query => {
+    setRecipes([]);
+    setPage(1);
+    setQuery(query);
+    setTotalRecipes(0);
+  };
+
+  const loadMore = () => {
+    setPage(prevPage => prevPage + 1);
+  };
+
+  const scrollOnLoadButton = () => {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: 'smooth',
+    });
+  };
+
+  const showLoadMore = recipes.length !== totalRecipes && !isLoading;
+
+  return (
+    <Container fixed maxWidth="desktop">
+      <PageTitle title="Search" />
+      <SearchBar onSubmit={onChangeQuery} />
+      <SearchTypeSelector onSelectChange={onSelectChange} />
+      <ul>
+        {recipes?.map(recipe => {
+          return <RecipeItem key={recipe.id} recipe={recipe} />;
+        })}
+      </ul>
+      {showLoadMore && <Button onClick={loadMore} />}
+    </Container>
+  );
 };
+
 export default SearchPage;
