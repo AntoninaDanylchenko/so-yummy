1633caf18c72af0ec991fe538d2b079fbc949572
Nadia Nechuiviter <nadiaejsmont@gmail.com>
2023-04-30 00:39:45 +0300

    pag

--- src/components/FavoriteList/FavoriteList.js
+++ src/components/FavoriteList/FavoriteList.js
@@ -1,35 +1,65 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 // import { ButtonDelete, LabelList, LabelListNumber } from './ContactList.styled';
 import { getFavorite } from 'redux/favorite/selector';
 import { getFavoriteOp } from 'redux/favorite/operation';
 // import { Loader } from 'components/Spinner/Spinner';
 import RecipeCard from 'components/RecipeCard/RecipeCard';
+import { Box, Pagination } from '@mui/material';
+import usePagination from '../PaginationComponent/Pagination';
+
+import PaginationComponent from 'components/PaginationComponent/PaginationComponent';
 
 const FavoriteList = () => {
-  const dispatch = useDispatch();
+  return (<PaginationComponent
+    getData={getFavorite}
+    getDataOp={getFavoriteOp}
+  />)
+  // const dispatch = useDispatch();
 
-  const { items: favorite } = useSelector(getFavorite);
+  // const { items: favorite } = useSelector(getFavorite);
 
-  useEffect(() => {
-    dispatch(getFavoriteOp());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(getFavoriteOp());
+  // }, [dispatch]);
 
-  return (
-    <ul>
-      {favorite.map(({ _id, title, description, preview, time }) => (
-        <li key={_id}>
-          <RecipeCard
-            id={_id}
-            title={title}
-            description={description}
-            preview={preview}
-            time={time}
-          />
-        </li>
-      ))}
-    </ul>
-  );
+  // let data = favorite;
+
+  // let [page, setPage] = useState(1);
+  // const PER_PAGE = 2;
+
+  // const count = Math.ceil(data.length / PER_PAGE);
+  // const _DATA = usePagination(data, PER_PAGE);
+
+  // const handleChange = (e, p) => {
+  //   setPage(p);
+  //   _DATA.jump(p);
+  // };
+
+  // return (
+  //   <>
+  //     <ul>
+  //       {_DATA.currentData().map(({ _id, title, description, preview, time }) => (
+  //         <li key={_id}>
+  //           <RecipeCard
+  //             id={_id}
+  //             title={title}
+  //             description={description}
+  //             preview={preview}
+  //             time={time}
+  //           />
+  //         </li>
+  //       ))}
+  //     </ul>
+  //     <Box>
+  //       <Pagination
+  //         count={count}
+  //         page={page}
+  //         onChange={handleChange}
+  //       />
+  //     </Box>
+  //   </>
+  // );
 };
 
 export default FavoriteList;
---
+++ src/components/PaginationComponent/PaginationComponent.js
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// import { Loader } from 'components/Spinner/Spinner';
+import RecipeCard from 'components/RecipeCard/RecipeCard';
+import { Box, Pagination } from '@mui/material';
+import usePagination from '../PaginationComponent/Pagination';
+
+const PaginationComponent = ({ getData, getDataOp }) => {
+    const dispatch = useDispatch();
+
+    const { items: favorite } = useSelector(getData); //getFavorite);
+
+    useEffect(() => {
+        dispatch(getDataOp());   //getFavoriteOp());
+    }, [dispatch, getDataOp]);
+
+    let data = favorite;
+
+    let [page, setPage] = useState(1);
+    const PER_PAGE = 2;
+
+    const count = Math.ceil(data.length / PER_PAGE);
+    const _DATA = usePagination(data, PER_PAGE);
+
+    const handleChange = (e, p) => {
+        setPage(p);
+        _DATA.jump(p);
+    };
+
+    return (
+        <>
+            <ul>
+                {_DATA.currentData().map(({ _id, title, description, preview, time }) => (
+                    <li key={_id}>
+                        <RecipeCard
+                            id={_id}
+                            title={title}
+                            description={description}
+                            preview={preview}
+                            time={time}
+                        />
+                    </li>
+                ))}
+            </ul>
+            <Box>
+                <Pagination
+                    count={count}
+                    page={page}
+                    onChange={handleChange}
+                />
+            </Box>
+        </>
+    );
+};
+
+export default PaginationComponent;
