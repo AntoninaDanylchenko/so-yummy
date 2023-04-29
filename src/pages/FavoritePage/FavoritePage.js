1bec816351ffc35e2a0270c7cdfc904e00363b22
Nadia Nechuiviter <nadiaejsmont@gmail.com>
2023-04-30 00:41:07 +0300

    fix

--- src/pages/FavoritePage/FavoritePage.js
+++ src/pages/FavoritePage/FavoritePage.js
@@ -1,14 +1,11 @@
 import FavoriteList from 'components/FavoriteList/FavoriteList';
 import Title from '../../components/Title/Title'
 import { Toaster } from 'react-hot-toast';
-
-import { useTheme } from '@mui/material';
 import {
   Container
 } from '../AddRecipePage/AddRecipePage.styled';
 
 const FavoritePage = () => {
-  const theme = useTheme();
   return (
     <Container >
       <Title title="Favorites" />
