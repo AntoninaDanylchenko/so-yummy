2eb116b1b25c0dd53353de172148f5d4e2ee68d4
Oksana-Pak <okspak7@gmail.com>
2023-05-01 01:10:40 +0300

    add loader to main and popular recipes

--- src/redux/popularRecipes/selectors.js
+++ src/redux/popularRecipes/selectors.js
@@ -1,1 +1,3 @@
 export const selectRecipesPopular = state => state.recipesPopular.recipes;
+export const selectIsLoading = state => state.recipesPopular.isLoading;
+export const selectError = state => state.recipesPopular.error;
