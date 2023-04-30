635c1632fff9856f18ce38b05fe4a6fba2b449ca
TosiaDan <antonina.ivanets@gmail.com>
2023-04-30 15:59:07 +0200

    corrects way for container

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -1,4 +1,4 @@
-import { Container } from '@mui/material';
+import { Container } from '../MainPage/MainPage.styled';
 import { useState, useEffect } from 'react';
 
 import PageTitle from 'components/ShoppingListPage/Title/PageTitle';
