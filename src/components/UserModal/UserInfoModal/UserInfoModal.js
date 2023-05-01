7f09c770d668bedb2a3466f7477216a2edc813be
Andrew Korchevskiy <37622720+Salamandero@users.noreply.github.com>
2023-05-01 15:35:32 +0300

    Update UserInfoModal.js

--- src/components/UserModal/UserInfoModal/UserInfoModal.js
+++ src/components/UserModal/UserInfoModal/UserInfoModal.js
@@ -88,12 +88,12 @@ const UserInfoModal = ({ onClose }) => {
           <FileInputWrap onChange={event => onFileInputChange(event)}>
             <label htmlFor="photo">
               <Image>
-                {!image && <UserImage src={avatarURL} alt="addphoto" />}
+                {!image && <UserImage src={avatarURL} alt="addphoto" loading="lazy"/>}
               </Image>
               <Plus />
             </label>
             <FileInput type="file" accept=".jpg, .jpeg, .png" id="photo" />
-            {image && <UserImage src={url} alt="userImage"></UserImage>}
+            {image && <UserImage src={url} alt="userImage" loading="lazy"></UserImage>}
           </FileInputWrap>
           <InputContainer>
             <IconUser />
