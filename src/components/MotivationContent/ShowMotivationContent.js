21c74adda63f4c96b840098833dc569a0e4bd0aa
ArtemKolesnyk <kolesnik2405@gmail.com>
2023-05-14 23:41:16 +0200

    modal-first-and-ten-favorit-recipe

--- src/components/MotivationContent/ShowMotivationContent.js
+++ src/components/MotivationContent/ShowMotivationContent.js
@@ -1,1 +1,8 @@
-
+/* background-image: url('${add300}'); */
+/* background-image: url('${addMob}'); */
+/* background-image: url('${addTab}'); */
+/* background-image: url('${addDesk}'); */
+/* background-image: url('${VectorMobMin}'); */
+/* background-image: url('${VectorMob}'); */
+/* background-image: url('${VectorTab}'); */
+/* background-image: url('${VectorDesk}'); */
